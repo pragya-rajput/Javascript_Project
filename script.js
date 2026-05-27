6989e85757c5b27148e3fbee85917b8c6e35af8f
Pragya Rajput <pragya.rajput2019@gmail.com>
2026-05-27 15:26:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 
+let myLibrary = [];
+
+function Book( authorName , title , category , description)
+{
+       this.bookId = crypto.randomUUID;
+       this.authorName = authorName;
+       this.title = title;
+       this.category = category;
+       this.description = description;
+}
+
+function addBooksToLibrary()
+{
+   const title = 
+     document.getElementById("Title").value;
+
+    const authorName = 
+      document.getElementById("authorName").value;
+
+    const category =
+       document.getElementById("category").value;
+
+   const description = 
+       document.getElementById("description").value;
+
+    const newBook = new Book(authorName , title , category , description);
+  
+
+     myLibrary.push(newBook);
+  
+}
+
+
+
